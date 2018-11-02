a847f82470d46b4f6291f311cf1679cc4bd789a6
João Luis da Silva Guio Soares <joaoluisguio@gmail.com>
2018-11-02 20:28:51 -0200

    prefabs,scenes,scripts: add obstacles and destroy mechanic

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+	[SerializeField] protected Vector2 velocity;
+
+	// Use this for initialization
+	// virtual protected void Start() {
+
+	// }
+
+	virtual protected void Spawn() {
+
+	}
+
+	virtual protected void Destroy() {
+		Destroy(this.gameObject);
+	}
+
+	protected void OnCollisionEnter2D(Collision2D col) {
+		print(col.gameObject.tag);
+		if (col.gameObject.CompareTag("destroy")) {
+			this.Destroy();
+		}
+	}
+}
