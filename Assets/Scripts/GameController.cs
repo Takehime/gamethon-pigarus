d7092160cdedf3276ee5f732ff005c7687620640
João Luis da Silva Guio Soares <joaoluisguio@gmail.com>
2018-11-03 13:30:38 -0200

    prefabs, scenes, tags: improve game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+[System.Serializable]
+public class PlayerData {
+    public Color color;
+}
 
 public class GameController : MonoBehaviour {
 
+    [SerializeField] GameObject victoryCounterContainer, victoryCounterMarkPrefab;
+    [SerializeField] int rounds = 5;
+    [SerializeField] List<PlayerData> playerData = new List<PlayerData>();
+    [SerializeField] float victoryMarkAppearDuration = 0.5f;
+
+    List<GameObject> victoryCounterMarkList = new List<GameObject>();
+    int currentRound = 0;
+
+    void InitializeVictoryCounter(GameObject container) {
+        if (container == null) {
+            Debug.Break();
+            print("victory counter not found");
+        }
+        for (int x = 1; x <= rounds; x++) {
+            GameObject tmp = Instantiate(victoryCounterMarkPrefab);
+            tmp.transform.SetParent(victoryCounterContainer.transform, false);
+            victoryCounterMarkList.Add(tmp);
+        }
+    }
+
+    void GiveRoundVictory(int playerId) {
+        var color = playerData[playerId].color;
+        victoryCounterMarkList[currentRound++].GetComponent<Image>().DOColor(color, victoryMarkAppearDuration);
+    }
+
+    IEnumerator GameLoop() {
+        yield break;
+    }
+
 	// Use this for initialization
 	void Start () {
-		
+        victoryCounterContainer = GameObject.FindWithTag("victory counter container");
+        InitializeVictoryCounter(victoryCounterContainer);
 	}
 	
 	// Update is called once per frame
