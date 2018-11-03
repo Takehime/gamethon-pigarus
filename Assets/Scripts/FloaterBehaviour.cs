6ce4274d4a01f5b60531cac029a12b0aaa839748
João Luis da Silva Guio Soares <joaoluisguio@gmail.com>
2018-11-03 12:17:32 -0200

    plugins, scenes, scripts: add floater squish via datuin son of alduin

--- Assets/Scripts/FloaterBehaviour.cs
+++ Assets/Scripts/FloaterBehaviour.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class FloaterBehaviour : ObstaclesBehaviour {
 
+    Vector3 originalScale;
+    [SerializeField] Vector2 tweenFactor = new Vector2(1.5f, 0.8f);
+    [SerializeField] float tweenSquishInDuration = 0.15f, tweenSquishOutDuration = 0.05f;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        originalScale = this.transform.localScale;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		GetComponent<Rigidbody2D>().velocity = velocity;
 	}
+
+    public void Squish() {
+        this.transform.DOScale(new Vector3(originalScale.x * tweenFactor.x, originalScale.y * tweenFactor.y), tweenSquishInDuration).OnComplete(() =>
+        {
+            this.transform.DOScale(originalScale, tweenSquishOutDuration);
+        });
+    }
 	
-	 public static void Spawn(GameObject baseObject, Vector2 baseVelocity) {
+	public static void Spawn(GameObject baseObject, Vector2 baseVelocity) {
 		GameObject newFloater = Instantiate(baseObject);
 		newFloater.GetComponent<FloaterBehaviour>().velocity = baseVelocity; 
 	}
