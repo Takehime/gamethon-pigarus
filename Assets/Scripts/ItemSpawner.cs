30d6032c768d075497418b50a5a5f3baebf24594
vinigarcia <vingarciasc@gmail.com>
2018-11-03 22:19:08 -0300

    Adding Wings (not used)

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+    public static ItemSpawner GetItemSpawner() {
+        var gc = GameObject.FindGameObjectWithTag("ItemSpawner");
+        if (gc == null || gc.GetComponentInChildren<ItemSpawner>() == null) {
+            Debug.Break();
+            print("ItemSpawner not found!");
+            return null;
+        } else {
+            return gc.GetComponentInChildren<ItemSpawner>();
+        }
+    }
+
+	public GameObject wingPrefab;
+	public Transform wingStartPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnWing(Vector3 position) {
+        var obj = Instantiate(wingPrefab);
+        obj.transform.position = position;
+	}
+}
